d6762d55f4da1fdd6b030ee9a420656c34660451
Mongkol_MM <mongkol2598@gmail.com>
2023-09-16 21:52:47 +0700

    fisrt commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# php-apache-mysql-docker
+
+## How to use 
+```
+docker-compose up
+```
+
+## How to login to phpmyadmin
+Use this username and password to login as root 
+
+Username 
+```
+root
+```
+Password
+```
+MYSQL_ROOT_PASSWORD
+```
+
+## How to create new folder
+You can create new project inside src folder 
+```
+php/src/[your-project]
+```
---
+++ php/src/index.php
@@ -0,0 +1,123 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="css/styles.css" rel="stylesheet" />
+    <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
+    <title>Document</title>
+</head>
+
+<body>
+
+    <?php require_once 'nav.php'; ?>
+    <div class="wrapper">
+        <span class="icon-close">
+            <ion-icon name="close"></ion-icon>
+        </span>
+        <div class="form-box login">
+            <h2>Login</h2>
+            <form action="#">
+                <div class="input-box">
+                    <span class="icon">
+                        <ion-icon name="mail"></ion-icon>
+                    </span>
+                    <input type="email" required>
+                    <label>Email</label>
+                </div>
+                <div class="input-box">
+                    <span class="icon">
+                        <ion-icon name="lock-closed"></ion-icon>
+                    </span>
+                    <input type="password" required>
+                    <label>Password</label>
+                </div>
+                <div class="remember-forgot">
+                    <label><input type="checkbox">Remember me</label>
+                    <a href="#">Forget Password</a>
+                </div>
+                <button type="submit" class="btn">Login</button>
+                <div class="login-register">
+                    <p>Don't have an account? <a href="#" class="register-link">Register</a></p>
+                </div>
+            </form>
+        </div>
+
+        <div class="form-box register">
+            <h2>Registration</h2>
+            <form action="#">
+                <div class="input-box">
+                    <span class="icon">
+                        <ion-icon name="person"></ion-icon>
+                    </span>
+                    <input type="text" required>
+                    <label>Username</label>
+                </div>
+                <div class="input-box">
+                    <span class="icon">
+                        <ion-icon name="mail"></ion-icon>
+                    </span>
+                    <input type="email" required>
+                    <label>Email</label>
+                </div>
+                <div class="input-box">
+                    <span class="icon">
+                        <ion-icon name="lock-closed"></ion-icon>
+                    </span>
+                    <input type="password" required>
+                    <label>Password</label>
+                </div>
+                <div class="remember-forgot">
+                    <label><input type="checkbox">agree to the terms & conditions</label>
+                </div>
+                <button type="submit" class="btn">Register</button>
+                <div class="login-register">
+                    <p>Already have an account? <a href="#" class="login-link">Login</a></p>
+                </div>
+            </form>
+        </div>
+
+    </div>
+
+
+
+
+
+    <!-- <?php
+        $host = 'db';
+        $user = 'root';
+        $pass = 'MYSQL_ROOT_PASSWORD';
+        $db = 'MYSQL_DATABASE';
+
+        $conn = new mysqli($host, $user, $pass, $db);
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+        } else {
+            echo "Connected to MySQL server successfully!";
+        }
+
+        $sql = "SELECT * FROM users";
+
+        if ($result = $conn->query($sql)) {
+            while($data = $result->fetch_object()) {
+                $users[] = $data;
+            }
+        }
+
+        echo "<ul>";
+        foreach($users as $user) {
+            echo "<li>";
+            echo $user->first_name . " " . $user->last_name . " " . $user->age;
+            echo "</li>";
+        }
+        echo "</ul>";
+    ?> -->
+
+    <script src="script.js"></script>
+    <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
+</body>
+
+</html>
